da4a73feccaefa6eaee6fe80af5cb1768292a046
LazySloth456 <eldon9163@gmail.com>
2022-04-13 15:50:00 +1200

    added main.py

---
+++ main/main.py
@@ -0,0 +1,52 @@
+import random
+
+# screen 21*8
+
+
+def ran():
+  return random.randint(1,5000)
+
+
+def screen(screenx,screeny):
+  map = []
+  for x in range(21):
+    for y in range(21):
+      random.seed(x+screenx+((screeny + y)*21))
+      randnum = ran()
+      if randnum < 7500:
+        map.append(" ")
+      elif randnum >= 7500 and randnum < 9995:
+        map.append("^")
+      elif randnum < 10001:
+        map.append("T")
+      else:
+        map.append("~")            
+  return map
+    
+print("starting infinite world!")
+
+camx = 128
+camy = 128
+pindex = 367
+pcollide = ["^"]
+
+while True:
+  map = screen(camx,camy)
+  map[pindex] = "A"
+  for y in range(21):
+    print(map[0+(y*21)]+map[1+(y*21)]+map[2+(y*21)]+map[3+(y*21)]+map[4+(y*21)]+map[5+(y*21)]+map[6+(y*21)]+map[7+(y*21)]+
+  map[8+(y*21)]+map[9+(y*21)]+map[10+(y*21)]+map[11+(y*21)]+map[12+y*21]+map[13+(y*21)]+map[14+(y*21)]+map[15+(y*21)]+
+  map[16+(y*21)]+map[17+(y*21)]+map[18+(y*21)]+map[19+(y*21)]+map[20+(y*21)])
+  pinput = input() + "m"
+  pinput = pinput[0]
+  if pinput == "a":
+    exit("game over!")
+  if pinput == "6" and not map[pindex+1] in pcollide[0:1]:
+    camy += 1
+  if pinput == "4" and not map[pindex-1] in pcollide[0:1]:
+    camy -= 1
+  if pinput == "8" and not map[pindex-21] in pcollide[0:1]:
+    camx -= 1
+  if pinput == "2" and not map[pindex+21] in pcollide[0:1]:
+    camx += 1    
+ 
