32e95ad54f0bbfb81d048f5d02b6b2576dbbbaf1
LazySloth456 <eldon9163@gmail.com>
2022-04-14 17:36:40 +1200

    added comments

--- main/main.py
+++ main/main.py
@@ -14,13 +14,12 @@ while True:
     map = terrain.generateCells(camy, camx)
     map[pindex] = "A"
     for y in range(32):
-        print(str(map[0 + (y * 32):32 + (y * 32)]).replace("\'", "", 99999).replace(",", "", 99999).replace("[",
-                                                                                                            "|").replace(
-            "]", "|"))  # Displays map to console
+        print(str(map[0 + (y * 32):32 + (y * 32)]).replace("\'", "", 99999).replace(",", "", 99999).replace("[","|").replace("]", "|"))  # Displays map to console
     pinput = input() + "m"
     pinput = pinput[0].lower()
     if pinput == "q":
         exit("game shut down")
+    # General controls for movement in game
     if pinput == "d" and not map[pindex + 1] in pcollide[0:len(pcollide)]:
         camx += 1
     if pinput == "a" and not map[pindex - 1] in pcollide[0:len(pcollide)]:
@@ -29,5 +28,5 @@ while True:
         camy -= 1
     if pinput == "s" and not map[pindex + 32] in pcollide[0:len(pcollide)]:
         camy += 1
-    if pinput == "e":
+    if pinput == "e":  # Opens the inventory so the player can use it.
         pass
