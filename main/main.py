74badcb7f4458656ab377d6988428a908b65173f
LazySloth456 <eldon9163@gmail.com>
2022-04-14 17:33:16 +1200

    organized

--- main/main.py
+++ main/main.py
@@ -1,29 +1,32 @@
 import terrainGeneration.generalTerrain
+import inventoryManager.inventory
 import cv2
 
-
 print("starting infinite world of AIDSrpg!")
-
 
 camx = 0
 camy = 0
 pindex = 496
-pcollide = ["∧", "🏠"] # If terrain character is in this array the player cannot move onto it.
+pcollide = ["∧", "🏠"]  # If terrain character is in this array the player cannot move onto it.
 
 while True:
-  map = terrainGeneration.generalTerrain.generateCells(camy,camx)
-  map[pindex] = "A"
-  for y in range(32):
-    print(str(map[0+(y*32):32+(y*32)]).replace("\'", "", 99999).replace(",", "", 99999).replace("[", "|").replace("]", "|")) # Displays map to console
-  pinput = input() + "m"
-  pinput = pinput[0].lower()
-  if pinput == "q":
-    exit("game shut down")
-  if pinput == "d" and not map[pindex+1] in pcollide[0:len(pcollide)]:
-    camx += 1
-  if pinput == "a" and not map[pindex-1] in pcollide[0:len(pcollide)]:
-    camx -= 1
-  if pinput == "w" and not map[pindex-32] in pcollide[0:len(pcollide)]:
-    camy -= 1
-  if pinput == "s" and not map[pindex+32] in pcollide[0:len(pcollide)]:
-    camy += 1
+    map = terrainGeneration.generalTerrain.generateCells(camy, camx)
+    map[pindex] = "A"
+    for y in range(32):
+        print(str(map[0 + (y * 32):32 + (y * 32)]).replace("\'", "", 99999).replace(",", "", 99999).replace("[",
+                                                                                                            "|").replace(
+            "]", "|"))  # Displays map to console
+    pinput = input() + "m"
+    pinput = pinput[0].lower()
+    if pinput == "q":
+        exit("game shut down")
+    if pinput == "d" and not map[pindex + 1] in pcollide[0:len(pcollide)]:
+        camx += 1
+    if pinput == "a" and not map[pindex - 1] in pcollide[0:len(pcollide)]:
+        camx -= 1
+    if pinput == "w" and not map[pindex - 32] in pcollide[0:len(pcollide)]:
+        camy -= 1
+    if pinput == "s" and not map[pindex + 32] in pcollide[0:len(pcollide)]:
+        camy += 1
+    if pinput == "e":
+        pass
--- main/terrainGeneration/generalTerrain.py
+++ main/terrainGeneration/generalTerrain.py
@@ -1,31 +1,32 @@
+# map generated is 32*32
 import random
-# map 32*32
 
 
 def ran():
-  return random.randint(1,5000) # Generates pseudo random number between 1 and 5000 for generation
+    return random.randint(1, 5000)  # Generates pseudo random number between 1 and 5000 for generation
 
 
 # Generates cells in square area offset by player position
 # This could be improved by generating only cells that are needed offset by player position when missing. 
-def generateCells(screenx,screeny):
-  map = [] # Start with blank map
-  for x in range(32):
-    for y in range(32):
-      random.seed(x+screenx+((y+screeny)*32)) # Get seed for cell in a real game might want to pass the number output as an md5 hash to make it more unpredictable.
-      randnum = ran() # generate cell number based off seed used
-      
-      if x+screenx > 5000 or x+screenx < -5000 or y+screeny < -5000 or y+screeny > 5000:       # World border, 5000 is world borders size in a direction
-        map.append("∧")
-      elif randnum < 4900:
-        map.append(" ")
-      elif randnum < 4982:
-        map.append("∧")
-      elif randnum < 5001:
-        map.append("🏠")
-      elif randnum < 4991:
-        map.append("")
+def generateCells(screenx, screeny):
+    map = []  # Start with blank map
+    for x in range(32):
+        for y in range(32):
+            random.seed(x + screenx + ((
+                                                   y + screeny) * 32))  # Get seed for cell in a real game might want to pass the number output as an md5 hash to make it more unpredictable.
+            randnum = ran()  # generate cell number based off seed used
 
-      else:
-        map.append("~")
-  return map # Returns generated map as output
+            if x + screenx > 5000 or x + screenx < -5000 or y + screeny < -5000 or y + screeny > 5000:  # World border, 5000 is world borders size in a direction
+                map.append("∧")
+            elif randnum < 4900:
+                map.append(" ")
+            elif randnum < 4982:
+                map.append("∧")
+            elif randnum < 5001:
+                map.append("🏠")
+            elif randnum < 4991:
+                map.append("")
+
+            else:
+                map.append("~")
+    return map  # Returns generated map as output
