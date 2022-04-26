e7d49ead672a147bf7de89ffff091e5a7d0cf5e3
dave <eldon9163@gmail.com>
2022-04-26 21:09:51 +1200

    started working on player inventory system.

--- main/inventory/inventoryManager.py
+++ main/inventory/inventoryManager.py
@@ -1,2 +1,19 @@
+import json
+import os
+
+
+def getPlayerInventory():
+    file = open("playerInventory.json", "r")
+    print("Loading player inventory data")
+    data = json.load(file)
+
+    print("Decoded data from json file")
+    file.close()
+
+
+def savePlayerInventory():
+    pass
+
+
 if __name__ == '__main__':
     exit(0)
