41f2ead9c63b360e08bc52fbadc5ff07809bd833
es19090 <es19090@melville-high.school.nz>
2022-05-16 14:26:11 +1200

    upgraded to pickle because it's just better.

--- main/inventory/inventoryManager.py
+++ main/inventory/inventoryManager.py
@@ -1,21 +1,19 @@
-import json
+import pickle
 import os
 
 def checkIfPlayerInventoryExistsAndCreateItIfItDoesNot():
     if os.path.exists(inventoryFileName):
         return True
-    file = open(inventoryFileName, "w")  # Creates file that didn't exist with basic template
-    file.close()
+    inventory = {"item1": "sword"}
+    with open(inventoryFileName, "wb") as file:
+        pickle.dump(inventory, file)
     return False
 
 
 def getPlayerInventory():
-    file = open(inventoryFileName, "r")
+    with open(inventoryFileName, "rb") as file:
+        inventory = pickle.load(file)
     print("Loading player inventory data")
-    data = json.load(file)
-
-    print(data)
-    file.close()
 
 
 def savePlayerInventory():
@@ -23,12 +21,13 @@ def savePlayerInventory():
 
 
 def openInventory():
-    with open(inventoryFileName) as f:  # opens the file again, works probably cause not >,'w'
-        items = json.load(f)  # saves the file into a variable
-    print('inventory contents:', items)  # prints the saved data
+    with open(inventoryFileName, "rb") as file:
+        inventory = pickle.load(file)
+    print("Loading player inventory data")
+    print('inventory contents:', inventory)  # prints the saved data
 
 
 if __name__ == '__main__':
     exit(0)
 else:
-    inventoryFileName = "playerInventory.json"
+    inventoryFileName = "playerInventory.bin"
