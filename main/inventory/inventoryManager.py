6b738de552ddef7aaeba1da730d9fd5de633c7cc
es19090 <es19090@melville-high.school.nz>
2022-05-16 14:38:49 +1200

    key system explained

--- main/inventory/inventoryManager.py
+++ main/inventory/inventoryManager.py
@@ -4,7 +4,7 @@ import os
 def checkIfPlayerInventoryExistsAndCreateItIfItDoesNot():
     if os.path.exists(inventoryFileName):
         return True
-    inventory = {"item1": "sword"}
+    inventory = {"item1": "sword", "item2": "item"}
     #print(inventory.keys()) use this to get possible functions to use with the dictionary
     with open(inventoryFileName, "wb") as file:
         pickle.dump(inventory, file)
@@ -27,6 +27,7 @@ def openInventory():
     print(inventory.keys())
     for key in inventory.keys():
         print(key)
+        print(inventory[key])
     print("Loading player inventory data")
     print('inventory contents:', inventory)  # prints the saved data
 
